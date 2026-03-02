52563683393635cb595dae4f7d9125cd3dfe94e9
MrIkso <mrikso821@gmail.com>
2026-03-02 19:46:57 +0200

    Fixed file extensions in file type detector

--- RDBExplorer/Utils/FileTypeDetector.cs
+++ RDBExplorer/Utils/FileTypeDetector.cs
@@ -49,39 +49,45 @@ namespace RDBExplorer.Utils
         private static readonly List<Signature> Signatures = new List<Signature>
         {
             new Signature("? 50 4E 47", ".png"),
-            new Signature("? 32 30 31 31 2D 30 34 2D 32 39 54 31 34 3A 32 36 3A 34 38 2B 30 39 30 30", ".bytecode"),
-            new Signature("00 22 04 20", ".database"),
-            AddAsciiSignature("DXBC", ".dxbc"),
-            AddAsciiSignature("_A1G", ".g1a"),
-            AddAsciiSignature("OC1G", ".g1co"),
-            AddAsciiSignature("ME1G", ".g1em"),
-            AddAsciiSignature("SE1G", ".g1es"),
-            AddAsciiSignature("XF1G", ".g1fx"),
-            AddAsciiSignature("_H1G", ".g1h"),
-            AddAsciiSignature("II1G", ".g1ii"),
-            AddAsciiSignature("_M1G", ".g1m"),
-            AddAsciiSignature("MN1G", ".g1nm"),
-            AddAsciiSignature("_N1G", ".g1n"),
-            AddAsciiSignature("FP1G", ".g1pf"),
-            AddAsciiSignature("2R1G", ".g1r2"),
-            AddAsciiSignature("GT1G", ".g1t"),
-            AddAsciiSignature("_A2G", ".g2a"),
-            AddAsciiSignature("_S2G", ".g2s"),
-            AddAsciiSignature("FBRG", ".grbf"),
-            AddAsciiSignature("_TGK", ".kgt"),
-            AddAsciiSignature("_RGK", ".kgr"),
-            AddAsciiSignature("_MHK", ".khm"),
-            AddAsciiSignature("_DOK", ".kod"),
-            AddAsciiSignature("LCSK", ".kscl"),
-            AddAsciiSignature("GSTK", ".ktsg"),
-            AddAsciiSignature("1FPO", ".opf1"),
-            AddAsciiSignature("BGIR", ".rigb"),
-            AddAsciiSignature("_DVS", ".svd"),
-            AddAsciiSignature("CGRS", ".srgc"),
-            AddAsciiSignature("ASRS", ".srsa"),
-            AddAsciiSignature("TSRS", ".srst"),
+            new Signature("? 32 30 31 31 2D 30 34 2D 32 39 54 31 34 3A 32 36 3A 34 38 2B 30 39 30 30", ".bytecode"), // Game comiled scripts
+            new Signature("00 22 04 20", ".database"), // might be game databases
+            //AddAsciiSignature("DXBC", ".dxbc"),       // unknown
+            AddAsciiSignature("_A2G", ".g1a"),        // G1AFile
+            AddAsciiSignature("OC1G", ".g1co"),       // Collision data
+            AddAsciiSignature("ME1G", ".g1em"),       // EffectMeshData
+            AddAsciiSignature("SE1G", ".g1es"),       // EffectShapeMeshData
+            AddAsciiSignature("XF1G", ".g1e"),        // EffectData
+            AddAsciiSignature("_H1G", ".g1h"),        // G1HFile
+            AddAsciiSignature("II1G", ".gii"),        // G1IIFile
+            AddAsciiSignature("_M1G", ".g1m"),        // ModelData
+            //AddAsciiSignature("MN1G", ".g1nm"),      // unknown
+            AddAsciiSignature("_N1G", ".g1n"),        // G1NFile
+            //AddAsciiSignature("FP1G", ".g1pf"),      // unknown
+            AddAsciiSignature("2R1G", ".rbg"),        // River2BakedGeometry
+            AddAsciiSignature("GT1G", ".g1t"),        // TexContext / StreamingTexContext
+            AddAsciiSignature("_A2G", ".g1a"),        // G1AFile (було .g2a)
+            AddAsciiSignature("_S2G", ".g1s"),        // G1SFile (було .g2s)
+            AddAsciiSignature("FBRG", ".grbf"),       // RBFData
+            //AddAsciiSignature("_TGK", ".kgt"),        // unknown
+            AddAsciiSignature("_RGK", ".kidsrender"), // RenderGraphFile
+            //AddAsciiSignature("_MHK", ".khm"),        // unknown
+            AddAsciiSignature("_DOK", ".kidsobjdb"),  // ObjectDatabaseFile
+            AddAsciiSignature("LCSK", ".kscl"),       // KSCLFile
+            AddAsciiSignature("GSTK", ".kts"),        // TexStageTableBinaryFile
+            AddAsciiSignature("1FPO", ".g1fpose"),    // FPoseData
+            AddAsciiSignature("BGIR", ".rigbin"),     // RigBinFile
+            //AddAsciiSignature("_DVS", ".svd"),        // unknown
+            AddAsciiSignature("CGRS", ".sgcbin"),     // GlobalConfiguration 
+            AddAsciiSignature("ASRS", ".srsa"),       // AssetData
+            AddAsciiSignature("TSRS", ".srst"),       // StreamAssetDataFile
+            AddAsciiSignature("XC1G", ".g1cox"),      // G1COXFile
+            AddAsciiSignature("1FRA", ".g1frani"),    // FRAnimationData
+            AddAsciiSignature("M1OK", ".g1mx"),       // G1MXFile
+            AddAsciiSignature("P1GK", ".g1p"),        // G1PFile
+            AddAsciiSignature("mmdb", ".mmdb"),       // MotionMatchingDatabase
+            AddAsciiSignature("SWGQ", ".swg"),        // SwingData
 
-            AddAsciiSignature("G2A_PACK", ".g2apack"),
+            AddAsciiSignature("G2A_PACK", ".g2apack"), // animation pack
             AddAsciiSignature("TMG_PACK", ".tmgpack"),
             AddAsciiSignature("TRMD", ".dmrt"),
             AddAsciiSignature("DMPP", ".dmpp"),
@@ -99,7 +105,7 @@ namespace RDBExplorer.Utils
             }
             else if (data.Length > 16)
             {
-               header = data.AsSpan(0, 16);
+                header = data.AsSpan(0, 16);
             }
             else
             {
