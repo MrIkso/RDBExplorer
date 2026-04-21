8183b0b54476dbf7f55b98d522a72a8e87257d1d
Ada Freya Ahmed <ada@chronovore.dev>
2026-04-22 00:53:52 +0530

    Update RDX entry struct

--- RDBExplorer/Core/Models/RdxEntry.cs
+++ RDBExplorer/Core/Models/RdxEntry.cs
@@ -3,7 +3,8 @@
     public struct RdxEntry
     {
         public ushort Index;
-        public ushort Marker;
+        public byte SubdirIndex; // if not 0xFF, path is {subdirs[subdirIndex]}/ instead of ./ (this is game dependent)
+        public byte LocaleIndex; // if not 0xFF, path is {selectedLanguage[localeIndex]}/0x{%08x}.fdata
         public uint FileId;
     }
 }
